9a71128a62a643b5b6b530b57db61f3e9caab45a
muhamadisro27 <isro.sabanur@trans-cosmos@co.id>
2023-10-12 13:33:20 +0700

    feat: map data categories

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
 const App = () => {
+  const categories = [
+    {
+      id: 1,
+      img: "#",
+      title: "Hats",
+    },
+    {
+      id: 2,
+      img: "#",
+      title: "Jacket",
+    },
+    {
+      id: 3,
+      img: "#",
+      title: "Sneakers",
+    },
+    {
+      id: 4,
+      img: "#",
+      title: "Womens",
+    },
+    {
+      id: 5,
+      img: "#",
+      title: "Mans",
+    },
+  ];
+
   return (
     <div className="categories-container">
-      <div className="category-container">
-        {/* img */}
-        <div className="category-body-container">
-          <h1>Hats</h1>
-          <p>Shop Now</p>
+      {categories.map((category) => (
+        <div className="category-container" key={category.id}>
+          <div className="background-image"></div>
+          {/* <img src={category.img} alt={category.title} /> */}
+          <div className="category-body-container">
+            <h1>{category.title}</h1>
+            <p>Shop Now</p>
+          </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 };
