51a21d4beff6c53d64618b244cc42e5ef8d109e3
muhamadisro27 <isro.sabanur@trans-cosmos@co.id>
2023-10-14 13:05:56 +0700

    feat: add new additional feature redirect

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,9 +1,31 @@
+// import { useEffect } from "react";
+// import { getRedirectResult } from "firebase/auth";
 import {
+  // auth,
   signInWithGooglePopup,
+  signInWithGoogleRedirect,
   createUserDocFromAuth,
 } from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
+  // useEffect(() => {
+  //   const fetchRedirect = async () => {
+  //     try {
+  //       const response = await getRedirectResult(auth);
+
+  //       if (response) {
+  //         await createUserDocFromAuth(response.user);
+  //       }
+
+  //       return;
+  //     } catch (error) {
+  //       console.log(error.message);
+  //     }
+  //   };
+
+  //   fetchRedirect();
+  // }, []);
+
   const logGoogleUser = async () => {
     try {
       const { user } = await signInWithGooglePopup();
@@ -12,10 +34,23 @@ const SignIn = () => {
       console.log(error);
     }
   };
+  const logGoogleUserRedirect = async () => {
+    try {
+      const { user } = await signInWithGoogleRedirect();
+      // await createUserDocFromAuth(user);
+      console.log(user);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
       <h1>Sign in</h1>
-      <button onClick={logGoogleUser}>Login using google</button>
+      <button onClick={logGoogleUser}>Sign in with google</button>
+      <button onClick={logGoogleUserRedirect}>
+        Sign in with google redirect
+      </button>
     </div>
   );
 };
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,6 +1,11 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  signInWithRedirect,
+} from "firebase/auth";
 
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
@@ -20,14 +25,17 @@ const firebaseConfig = {
 // Initialize Firebase
 initializeApp(firebaseConfig);
 
-const provider = new GoogleAuthProvider();
+const googleProvider = new GoogleAuthProvider();
 
-provider.setCustomParameters({
+googleProvider.setCustomParameters({
   prompt: "select_account",
 });
 
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, googleProvider);
 
 export const db = getFirestore();
 
