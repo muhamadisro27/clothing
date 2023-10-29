3e1e92584ba9ee56ff41d6abe0dca0d0e1f3b959
muhamadisro27 <isro.sabanur@trans-cosmos@co.id>
2023-10-29 22:13:31 +0700

    feat: add helper function

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,9 +1,10 @@
 import { useReducer } from "react";
-import { createContext, useEffect, useState } from "react";
+import { createContext, useEffect } from "react";
 import {
   createUserDocFromAuth,
   onAuthStateChangedListener,
 } from "../utils/firebase/firebase.utils";
+import { createAction } from "../utils/reducer/reducer.utils.ts";
 
 export const UserContext = createContext({
   currentUser: null,
@@ -36,7 +37,7 @@ export const UserProvider = ({ children }) => {
   const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
 
   const setCurrentUser = (user) => {
-    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+    dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
   };
 
   const value = { currentUser, setCurrentUser };
