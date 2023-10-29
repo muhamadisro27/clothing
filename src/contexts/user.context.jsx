202727a220cb5b7c8e7e194ad753f9472a459a08
muhamadisro27 <isro.sabanur@trans-cosmos@co.id>
2023-10-29 20:09:47 +0700

    feat: change usestate to usereducer

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,3 +1,4 @@
+import { useReducer } from "react";
 import { createContext, useEffect, useState } from "react";
 import {
   createUserDocFromAuth,
@@ -9,8 +10,35 @@ export const UserContext = createContext({
   setCurrentUser: () => null,
 });
 
+const INITIAL_STATE = {
+  currentUser: null,
+};
+
+const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: "SET_CURRENT_USER",
+};
+
+const userReducer = (state, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: payload,
+      };
+    default:
+      throw new Error(`Invalid action`);
+  }
+};
+
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
+  const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
+
+  const setCurrentUser = (user) => {
+    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+  };
+
   const value = { currentUser, setCurrentUser };
 
   useEffect(() => {
