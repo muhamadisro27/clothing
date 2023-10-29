e92cebc8b305fac2ceb9a300f66c8b3a359d2bed
muhamadisro27 <isro.sabanur@trans-cosmos@co.id>
2023-10-29 15:36:38 +0700

    feat: change to styled signup component

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-import "./sign-up-form.style.scss";
+import {SignUpContainer} from "./sign-up-form.style.jsx";
 
 import {
   createAuthUserWithEmailAndPassword,
@@ -64,7 +64,7 @@ const SignUpForm = () => {
   }
 
   return (
-    <div className="sign-up-container">
+    <SignUpContainer>
       <h2>Don't have an account?</h2>
       <span>Sign up with your email and password</span>
       <form onSubmit={(e) => submitData(e)}>
@@ -117,7 +117,7 @@ const SignUpForm = () => {
           Sign Up
         </Button>
       </form>
-    </div>
+    </SignUpContainer>
   );
 };
 
