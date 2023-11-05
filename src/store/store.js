d67800ad95425bcf39435703553603bb772bcaae
muhamadisro27 <isro.sabanur@trans-cosmos@co.id>
2023-11-05 18:16:32 +0700

    feat: add redux devtools

--- src/store/store.js
+++ src/store/store.js
@@ -12,9 +12,17 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middlewares = [process.env.NODE_ENV === "development" && logger].filter(Boolean);
+const middlewares = [process.env.NODE_ENV !== "production" && logger].filter(
+  Boolean
+);
 
-const composedEnhancers = compose(applyMiddleware(...middlewares));
+const composeEnhancer =
+  (process.env.NODE_ENV !== "production" &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middlewares));
 
 export const store = createStore(
   persistedReducer,
