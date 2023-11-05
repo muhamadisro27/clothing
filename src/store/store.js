c2e3e5e5cc893fc359aacc50f9d5fd1b07c458fd
muhamadisro27 <isro.sabanur@trans-cosmos@co.id>
2023-11-05 19:01:54 +0700

    feat: add categories preview loading spinner

--- src/store/store.js
+++ src/store/store.js
@@ -8,14 +8,15 @@ import thunk from "redux-thunk";
 const persistConfig = {
   key: "root",
   storage,
-  blacklist: ["user"],
+  whitelist: ["cart"],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middlewares = [process.env.NODE_ENV !== "production" && logger, thunk].filter(
-  Boolean
-);
+const middlewares = [
+  process.env.NODE_ENV !== "production" && logger,
+  thunk,
+].filter(Boolean);
 
 const composeEnhancer =
   (process.env.NODE_ENV !== "production" &&
